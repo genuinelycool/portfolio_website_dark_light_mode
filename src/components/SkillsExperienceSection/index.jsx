20ae29c8ff65f1a569f7c2c03cfc4d8122b4b540
genuinelycool <genuinelycool@gmail.com>
2026-01-15 04:04:48 +0545

    13 - Building the Skills & Experience Section Layout

--- src/components/SkillsExperienceSection/index.jsx
+++ src/components/SkillsExperienceSection/index.jsx
@@ -4,18 +4,22 @@ import ExperienceItem from "./ExperienceItem";
 const SkillsExperienceSection = () => {
   return (
     <section className="min-h-screen py-20">
-      <h1>SkillsExperienceSection Section.</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <h2 className="text-3xl font-semibold text-center">
+        Skills & Experience
+      </h2>
+      <h3 className="text-4xl font-bold text-center mb-16 text-blue-600 dark:text-blue-400">
+        What I Do & Where I ve Been
+      </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 max-w-6xl max-auto">
+        {/* Skills */}
+        <div>
+          <h4 className="text-2xl font-semibold mb-4">Skills</h4>
+        </div>
+        {/* Experience Timeline */}
+        <div>
+          <h4 className="text-2xl font-semibold mb-4">Experience</h4>
+        </div>
+      </div>
     </section>
   );
 };
